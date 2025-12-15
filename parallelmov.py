4d853561508e53cb9253ef29b10ee859f94c228d
Aidan Wong <anominosgamer@gmail.com>
2025-12-15 14:10:48 +0000

    changes

--- parallelmov.py
+++ parallelmov.py
@@ -1,50 +1,35 @@
-# Basic example to swap two registers using ParallelMov
+# Generate filecheck test for a parallel move
 
+from collections.abc import Sequence
 from xdsl.context import Context
-from xdsl.dialects.builtin import ModuleOp, ArrayAttr
-from xdsl.dialects.riscv import RISCV, ParallelMovOp, GetRegisterOp, Registers, IntRegisterType
+from xdsl.dialects.builtin import ModuleOp, ArrayAttr, Operation, SSAValue, Attribute
+from xdsl.dialects.riscv import RISCV, ParallelMovOp, GetRegisterOp, CustomAssemblyInstructionOp
+from xdsl.dialects.test import TestOp
+
+from xdsl.dialects.riscv import Registers as R
 from xdsl.transforms import riscv_lower_parallel_mov
 # Initialize context and load dialects
 ctx = Context()
 ctx.load_dialect(RISCV)
 
-s1 = GetRegisterOp(Registers.S1)
-s2 = GetRegisterOp(Registers.S2)
 
-module = ModuleOp([
-    s1, s2,
-    ParallelMovOp([s1, s2], [Registers.S1, Registers.S2], None),  # 2nd example (noop)
-    # ParallelMovOp([s1, s2], [Registers.S3, Registers.S4], ArrayAttr([Registers.S5]))  # first example below
-])
+inps = [R.S1, R.S2, R.S3]
+outs = [R.S2, R.S3, R.S1]
+free_reg = ArrayAttr([R.S10])
+assert len(inps) == len(outs)
+
+l: list[Operation] = [TestOp([], inps)]
+
+l.append(ParallelMovOp(l[0].results, outs))
+
+l.append(TestOp(l[-1].results, []))
+
+module = ModuleOp(l)
 module.verify()
 print(module)
 
-print("-----LOWERING-----")
-riscv_lower_parallel_mov.RISCVLowerParallelMovPass().apply(None, module)
-print("---END LOWERING---")
+# print("-----LOWERING-----")
+# riscv_lower_parallel_mov.RISCVLowerParallelMovPass().apply(None, module)
+# print("---END LOWERING---")
+
 # print(module)
-
-
-
-"""
-%2, %3 = mov %1, %0 : a0, a1 -> a2, a3
-
-%2 = mv %1 : a0 -> a2
-%3 = mv %0 : a1 -> a3
-
-
-
-%2, %3 = mov %1, %0 : a0, a1 -> a0, a1
-
-%2 = mv %1 : a0 -> a0
-%2 = %1
-
-
-
-%2, %3 = mov %1, %0 : a0, a1 -> a1, a0
-
-%4 = mv %1 : a0 -> a2
-%3 = mv %0 : a1 -> a0
-%2 = mv %4 : a2 -> a1
-
-"""
